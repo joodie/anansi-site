73b0da32cae425385a20e3937a17da19c42147a7
Joost Diepenmaat <joost@zeekat.nl>
2010-11-25 13:49:14 +0100

    specify zeekat-utils dependency

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [ring/ring-core "0.3.1"]
                  [ring/ring-jetty-adapter "0.3.1"]
                  [ring/ring-devel "0.3.1"]
-                 [clj-stacktrace "0.2.0"]]
+                 [clj-stacktrace "0.2.0"]
+                 [zeekat-utils "0.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [clojure-refactoring "0.4.0"]])
