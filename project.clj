d518efb335ebce5d7cfee1bb1b1b842b8a5a3969
Joost Diepenmaat <joost@zeekat.nl>
2011-03-18 11:08:52 +0100

    v0.0.4

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject anansi-site "0.0.3"
+(defproject anansi-site "0.0.4"
   :description "basic compojure/ring based web framework"
-  :dependencies [[anansi-handlers "1.0.0-SNAPSHOT"]
+  :dependencies [[anansi-handlers "1.0.1"]
                  [hiccup "0.3.2"]
                  [swank-clojure "1.2.1"]
-                 [clj-time "0.1.0-SNAPSHOT"]
+                 [org.clojars.retiman/clj-time "0.3.0"]
                  [ring/ring-core "0.3.1"]
                  [ring/ring-jetty-adapter "0.3.1"]
                  [ring/ring-devel "0.3.1"]
